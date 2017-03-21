aac2f73067a6cf32e4c395d0c5cc227da752eb44
jayala-29 <jayala@ucsd.edu>
2017-03-21 15:45:33 -0700

    program that encrypts a message using pi (only letters and spaces)

---
+++ PiDay2016.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class PiDay2016Encryptor {
+
+  public static void main(String[] args) {
+  
+    Scanner input = new Scanner(System.in);
+    
+    int n = input.nextInt();
+    
+    String message = input.nextLine();
+    message.toLowerCase();
+    
+    char[] cMessage = char[n];
+    
+    // a b -> 3.14 3.143.14
+    // ab ba -> 3.14|3.143.14 3.143.14|3.14
+    // ...
+    for (int i = 0; i < n; i++) {
+    
+      char[i] = message.charAt(i);
+      
+      // if a space in the message, just print a space
+      if (char[i] != 32) {
+        
+        // print π-position-in-the-alphabet number of times
+        // a -> 3.14
+        // b -> 3.143.14
+        // c -> 3.143.143.14
+        // ...
+        for (int j = 96; j < char[i]; j++) {
+      
+          System.out.print("3.14");
+        }
+      
+        System.out.print("|");
+    
+      else {
+    
+        System.out.print(" ");
+      }
+    }
+  }
+}
