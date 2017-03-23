62b28b170cdec52ac96695f1cd877adb33ec561e
jayala-29 <jayala@ucsd.edu>
2017-03-22 17:39:01 -0700

    Update PiDay2016Encryptor.java

--- PiDay2016Encryptor.java
+++ PiDay2016Encryptor.java
@@ -5,6 +5,7 @@ public class PiDay2016Encryptor {
   public static void main(String[] args) {
   
     // encrypts message only containing letters and spaces
+    System.out.print("Please enter a message to encrypt (first put in length including spaces): ");
     Scanner input = new Scanner(System.in);
     
     int n = input.nextInt();
@@ -12,29 +13,30 @@ public class PiDay2016Encryptor {
     String message = input.nextLine();
     message.toLowerCase();
     
-    char[] cMessage = char[n];
+    char[] cMessage = new char[n];
     
-    // a b -> 3.14 3.143.14
-    // ab ba -> 3.14|3.143.14 3.143.14|3.14
+    // 4 a b ->  3.14 3.143.14
+    // 6 ab ba ->  3.14|3.143.14 3.143.14|3.14
     // ...
     for (int i = 0; i < n; i++) {
     
-      char[i] = message.charAt(i);
+      cMessage[i] = message.charAt(i);
       
       // if a space in the message, just print a space
-      if (char[i] != 32) {
+      if (cMessage[i] != 32) {
         
         // print π-position-in-the-alphabet number of times
-        // a -> 3.14
-        // b -> 3.143.14
-        // c -> 3.143.143.14
+        // 2 a ->  3.14
+        // 2 b ->  3.143.14
+        // 2 c ->  3.143.143.14
         // ...
-        for (int j = 96; j < char[i]; j++) {
+        for (int j = 96; j < cMessage[i]; j++) {
       
           System.out.print("3.14");
         }
-      
+        
         System.out.print("|");
+      }
     
       else {
     
